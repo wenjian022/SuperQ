44c9e94fbc91e872abff7d57ac2edd17d8ac1747
王文建 <wangwenjian@newings.net.cn>
2024-11-12 14:22:34 +0800

    初始化

---
+++ src/config/defaultSettings.js
@@ -0,0 +1,33 @@
+/**
+ * 项目默认配置项
+ * primaryColor - 默认主题色, 如果修改颜色不生效，请清理 localStorage
+ * navTheme - sidebar theme ['dark', 'light'] 两种主题
+ * colorWeak - 色盲模式
+ * layout - 整体布局方式 ['sidemenu', 'topmenu'] 两种布局
+ * fixedHeader - 固定 Header : boolean
+ * fixSiderbar - 固定左侧菜单栏 ： boolean
+ * contentWidth - 内容区布局： 流式 |  固定
+ *
+ * storageOptions: {} - Vue-ls 插件配置项 (localStorage/sessionStorage)
+ *
+ */
+
+export default {
+  navTheme: 'dark',
+  primaryColor: '#1890FF',
+  layout: 'sidemenu',
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  autoHideHeader: false,
+  fixSiderbar: true,
+  hideHintAlert: false,
+  hideCopyButton: false,
+  menu: {
+    disableLocal: false
+  },
+  title: 'Guardian',
+  pwa: false,
+  iconfontUrl: '',
+  collapse: true,
+  production: true
+}
---
+++ src/views/user/Login.vue
@@ -0,0 +1,212 @@
+<template>
+  <div class="main">
+    <a-form
+      id="formLogin"
+      class="user-layout-login"
+      ref="formLogin"
+      :form="form"
+      @submit="handleSubmit"
+    >
+      <a-tabs
+        :activeKey="customActiveKey"
+        :tabBarStyle="{ textAlign: 'center', borderBottom: 'unset' }"
+        @change="handleTabClick"
+      >
+        <a-tab-pane key="tab1" :tab="$t('user.login.tab-login-credentials')">
+          <a-alert
+            v-if="isLoginError"
+            type="error"
+            showIcon
+            style="margin-bottom: 24px;"
+            :message="$t('user.login.message-invalid-credentials')" />
+          <a-form-item>
+            <a-input
+              size="large"
+              type="text"
+              placeholder="账号"
+              v-decorator="[
+                'username',
+                {rules: [{ required: true, message:'请输入账号!'}, { validator: handleUsernameOrEmail }], validateTrigger: 'change'}
+              ]"
+            >
+              <a-icon slot="prefix" type="user" :style="{ color: 'rgba(0,0,0,.25)' }" />
+            </a-input>
+          </a-form-item>
+
+          <a-form-item>
+            <a-input-password
+              size="large"
+              placeholder="密码"
+              v-decorator="[
+                'password',
+                {rules: [{ required: true, message: '请输入密码!' }], validateTrigger: 'blur'}
+              ]"
+            >
+              <a-icon slot="prefix" type="lock" :style="{ color: 'rgba(0,0,0,.25)' }" />
+            </a-input-password>
+          </a-form-item>
+
+          <!--          <a-form-item>-->
+          <!--            <a-input-->
+          <!--              size="large"-->
+          <!--              placeholder="认证码"-->
+          <!--              autocomplete="off"-->
+          <!--              v-decorator="[-->
+          <!--                'dualAuth',-->
+          <!--                {rules: [{ required: true, message: '请输入认证码!' }], validateTrigger: 'blur'}-->
+          <!--              ]"-->
+          <!--            >-->
+          <!--              <a-icon slot="prefix" type="code" :style="{ color: 'rgba(0,0,0,.25)' }" />-->
+          <!--            </a-input>-->
+          <!--          </a-form-item>-->
+
+        </a-tab-pane>
+      </a-tabs>
+
+      <a-form-item style="margin-top:24px">
+        <a-button
+          size="large"
+          type="primary"
+          htmlType="submit"
+          class="login-button"
+          :loading="state.loginBtn"
+          :disabled="state.loginBtn"
+        >{{ $t('user.login.login') }}
+        </a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script>
+import TwoStepCaptcha from '@/components/tools/TwoStepCaptcha'
+import { mapActions } from 'vuex'
+import { timeFix } from '@/utils/util'
+
+export default {
+  components: {
+    TwoStepCaptcha
+  },
+  data () {
+    return {
+      customActiveKey: 'tab1',
+      loginBtn: false,
+      loginType: 0,
+      isLoginError: false,
+      requiredTwoStepCaptcha: false,
+      stepCaptchaVisible: false,
+      initializationUser: false,
+      form: this.$form.createForm(this),
+      state: {
+        time: 60,
+        loginBtn: false,
+        loginType: 0,
+        smsSendBtn: false
+      }
+    }
+  },
+  methods: {
+    ...mapActions(['Login', 'Logout']),
+    // handler
+    handleUsernameOrEmail (rule, value, callback) {
+      const { state } = this
+      const regex = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/
+      if (regex.test(value)) {
+        state.loginType = 0
+      } else {
+        state.loginType = 1
+      }
+      callback()
+    },
+    handleTabClick (key) {
+      this.customActiveKey = key
+    },
+    handleSubmit (e) {
+      e.preventDefault()
+      const {
+        form: { validateFields },
+        state,
+        customActiveKey,
+        Login
+      } = this
+
+      state.loginBtn = true
+
+      const validateFieldsKey = customActiveKey === 'tab1' ? ['username', 'password', 'dualAuth'] : ['mobile', 'captcha']
+
+      validateFields(validateFieldsKey, { force: true }, async (err, values) => {
+        if (!err) {
+          const loginParams = { ...values }
+          Login(loginParams).then((res) => this.loginSuccess(res))
+            .finally(() => {
+              state.loginBtn = false
+            })
+        } else {
+          setTimeout(() => {
+            state.loginBtn = false
+          }, 600)
+        }
+      })
+    },
+    loginSuccess (res) {
+      this.$router.push({ path: '/' })
+      // 延迟 1 秒显示欢迎信息
+      setTimeout(() => {
+        this.$notification.success({
+          message: '欢迎',
+          description: `${timeFix()}，欢迎回来`
+        })
+      }, 1000)
+      this.isLoginError = false
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.user-layout-login {
+  label {
+    font-size: 14px;
+  }
+
+  .getCaptcha {
+    display: block;
+    width: 100%;
+    height: 40px;
+  }
+
+  .forge-password {
+    font-size: 14px;
+  }
+
+  button.login-button {
+    padding: 0 15px;
+    font-size: 16px;
+    height: 40px;
+    width: 100%;
+  }
+
+  .user-login-other {
+    text-align: left;
+    margin-top: 24px;
+    line-height: 22px;
+
+    .item-icon {
+      font-size: 24px;
+      color: rgba(0, 0, 0, 0.2);
+      margin-left: 16px;
+      vertical-align: middle;
+      cursor: pointer;
+      transition: color 0.3s;
+
+      &:hover {
+        color: #1890ff;
+      }
+    }
+
+    .register {
+      float: right;
+    }
+  }
+}
+</style>
